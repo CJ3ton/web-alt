1a1291fe1df856a4c7ab64e0876c7e178650f15e
cj3ton <cj3ton@gmail.com>
2022-04-04 00:34:11 +0700

    Desktop version added 2 sections

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,19 @@
 const arrows = document.querySelectorAll('.how__item-arrow'),
     howText = document.querySelectorAll('.how__item-text'),
-    howItems = document.querySelectorAll('.how__item');
+    howItems = document.querySelectorAll('.how__item'),
+    tabs = document.querySelectorAll('.feat__tabs-tab'),
+    tabsBtn = document.querySelectorAll('.feat__tabs-nav li a');
 
-console.log(howItems);
+console.log(tabs);
 
-    arrows.forEach((arrow, i) => {
+arrows.forEach((arrow, i) => {
     arrow.addEventListener('click', (e) => {
-        if(howItems[i].clientHeight == 70) {
+        if (howItems[i].clientHeight == 70) {
             howItems[i].style.height = (70 + howText[i].clientHeight) + 'px';
         } else {
             howItems[i].style.height = '70px';
         }
-        
+
         arrows[i].classList.toggle('active');
         howText[i].classList.toggle('open');
         console.log(howItems[i].clientHeight);
@@ -28,4 +30,29 @@ const swiper = new Swiper('.swiper', {
         nextEl: '.clients__slider-next',
         prevEl: '.clients__slider-prev',
     },
+});
+
+function hideTabContent() {
+    tabs.forEach(tab => {
+        tab.classList.remove('feat__tabs-active');
+    });
+    tabsBtn.forEach(btn => {
+        btn.classList.remove('btn__active');
+    });
+}
+
+function showTabContent(i = 0) {
+    tabs[i].classList.add('feat__tabs-active');
+    tabsBtn[i].classList.add('btn__active');
+}
+
+hideTabContent();
+showTabContent();
+
+tabsBtn.forEach((btn, i)=> {
+    btn.addEventListener('click', e => {
+        e.preventDefault();
+        hideTabContent();
+        showTabContent(i);
+    });
 });
